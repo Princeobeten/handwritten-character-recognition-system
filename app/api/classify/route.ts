c3e7b431ac903edbf44349f2652d4cdb437d8390
princeobeten <princeobeten56@gmail.com>
2025-04-12 10:59:26 +0100

    fix 2

--- app/api/classify/route.ts
+++ app/api/classify/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { knnClassifier } from '@/lib/classifier';
 import prisma from '@/lib/prisma';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../auth/[...nextauth]/route';
+import { authOptions } from "../../api/auth/auth.config";
 
 export async function POST(request: NextRequest) {
   try {
